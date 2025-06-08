189b07dfc7e20c5f08f881921b69d90453f130c4
tarinkun <ta5ku6ma@docomo.ne.jp>
2025-06-08 18:13:31 +0900

    main.cの編集

--- main.c
+++ main.c
@@ -1,28 +1,18 @@
+#include <vita2d.h> // vita2dlibインクルード
+#include <psp2/ctrl.h> // コントローラー入力
+#include <psp2/kernel/processmgr.h> // スリープ
 
-#include <vitasdk.h> // VitaSDKをインクルード
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 544
 
 // アプリケーションのエントリポイント
 int main(int argc, char *argv[]) {
-    // スクリーンを初期化
-    sceDisplaySetFrameBuf(
-        &(SceDisplayFrameBuf){
-            .size = sizeof(SceDisplayFrameBuf),
-            .base = 0x00000000,
-            .pitch = 960,
-            .pixelformat = SCE_DISPLAY_PIXELFORMAT_A8B8G8R8,
-            .common = SCE_DISPLAY_SETBUF_CONT_WIDTH | SCE_DISPLAY_SETBUF_CONT_HEIGHT
-        }, SCE_DISPLAY_SETBUF_NEXTFRAME);
-    SceAppUtilInitParam init_param;
-    SceAppUtilBootParam boot_param;
-    memset(&init_param, 0, sizeof(SceAppUtilInitParam));
-    memset(&boot_param, 0, sizeof(SceAppUtilBootParam));
-    sceAppUtilInit(&init_param, &boot_param);
+    // vita2dlibの初期化
+    vita2d_init();
+    // コントローラーの初期化
+    sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
 
-    
-    printf("tarinkun start\n");
-
-  
-    //
+    // メインループ
     while (1) {
         SceCtrlData pad;
         sceCtrlReadBufferPositive(0, &pad, 1);
@@ -32,12 +22,29 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        // CPU使用率を下げる
-        sceKernelDelayThread(1000 * 1000 / 60); // 
+        // 描画開始
+        vita2d_start_drawing();
+        // 背景を黒でクリア
+        vita2d_clear_screen();
+
+        // 文字列「Hello World!」を描画
+        // vita2d_font_draw_textf(font_ptr, x, y, color, scale, "フォーマット文字列", ...)
+        // フォントがロードされていないので、デフォルトのフォントを使う
+        // デフォルトフォントで日本語が表示されるかは環境による
+        vita2d_draw_string(100, 100, RGBA8(255, 255, 255, 255), 2.0f, "tarinkun vitaset start"); // 白で表示
+
+        // 描画終了
+        vita2d_end_drawing();
+        // 画面をフリップ（描画された内容を表示）
+        vita2d_swap_buffers();
+
+        //CPU使用率を下げる
+        sceKernelDelayThread(1000 * 1000 / 60);
     }
 
-    // クリーンアップ
-    sceAppUtilShutdown();
-
-    return 0; // 終了
+    // vita2dlibの終了
+    vita2d_fini();
+    // アプリケーション終了
+    sceKernelExitProcess(0);
+    return 0;
 }
